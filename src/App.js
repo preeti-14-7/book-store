6509e52871a19d82ad38dd3b49c7771e727af5c0
Wasim Raja <wasim7raja10@gmail.com>
2023-03-18 11:28:52 +0000

    done

--- src/App.js
+++ src/App.js
@@ -4,20 +4,22 @@ import Home from './Home/Home';
 
 import './App.css';
 
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import React from 'react';
+
 function App() {
   return (
     <div className="App">
       <Router>
-    <div className="App">
-            <Routes>
-              <Route exact path="/" element={<LogIn />} />
-  
-              <Route path="/home" element={<Home />} />
-           
-            </Routes>
-        
-      </div>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<LogIn />} />
+
+            <Route path="/home" element={<Home />} />
+
+          </Routes>
+
+        </div>
       </Router>
     </div>
   );
--- src/Auth/LogIn.js
+++ src/Auth/LogIn.js
@@ -1,3 +1,4 @@
+import React from "react";
 import "./LogIn.css";
 
 
@@ -6,43 +7,43 @@ function LogIn() {
   return (
     <div className="top ">
       <h2 className="mt-1 mb-5">LogIn to continue</h2>
-    <div class="row justify-content-center align-items-center ">
-      <div class="col-md-4">
- 
-    <form  className = "form" >
-      <div className="form-outline mb-4">
-        <input
-          type="email"
-          class="form-control"
-          placeholder="Email"
-          id="email"
-         
-          
-        />
-        
+      <div className="row justify-content-center align-items-center ">
+        <div className="col-md-4">
+
+          <form className="form" >
+            <div className="form-outline mb-4">
+              <input
+                type="email"
+                className="form-control"
+                placeholder="Email"
+                id="email"
+
+
+              />
+
+            </div>
+            <div className="form-outline mb-4">
+              <input
+                type="password"
+                id="password"
+                placeholder="Password"
+                className="form-control"
+
+              />
+
+
+            </div>
+            <div className="row d-flex gap-2">
+              <button className="close-btn btn btn-block mb-4 col-lg-12" type="submit">submit</button>
+              <p className="text-center"> OR</p>
+              <button className=" btn btn-danger btn-lg col-lg-12">continue with Google</button>
+              <button className="btn btn-primary btn-lg col-lg-12">continue with Facebook</button>
+              <button className="btn btn-danger btn-lg col-lg-12">continue with Gmail</button>
+            </div>
+          </form>
+
+        </div>
       </div>
-      <div className="form-outline mb-4">
-        <input
-          type="password"
-          id="password"
-          placeholder="Password"
-          class="form-control"
-         
-        />
-       
-     
-      </div>
-      <div className="row d-flex gap-2">
-      <button className="close-btn btn btn-block mb-4 col-lg-12" type="submit">submit</button>
-      <p className="text-center"> OR</p>
-      <button  className = " btn btn-danger btn-lg col-lg-12" type="click">continue with Google</button>
-      <button  className = "btn btn-primary btn-lg col-lg-12" type="click">continue with Facebook</button>
-      <button  className = "btn btn-danger btn-lg col-lg-12" type="click">continue with Gmail</button>
-      </div>
-    </form>
-  
-    </div>
-    </div>
     </div>
   );
 }
